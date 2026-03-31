efd7bf2aef19b9b23d395ec0c73e63c90113b03a
Kevin Ortega Chávez <a23490375@itmexicali.edu.mx>
2026-03-30 20:56:04 -0700

    feat: completar nivel 4 - validar_orden_descendente

--- calculadora/validaciones/orden_descendente.py
+++ calculadora/validaciones/orden_descendente.py
@@ -44,4 +44,20 @@ def validar_orden_descendente(cadena: str) -> bool:
         >>> validar_orden_descendente("VIV")
         False
     """
-    raise NotImplementedError()
+    valores = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sustracciones_validas = {'IV', 'IX', 'XL', 'XC', 'CD', 'CM'}
+    i = 0
+    while i < len(cadena) - 1:
+        par = cadena[i:i+2]
+        if par in sustracciones_validas:
+            if i > 0 and cadena[i - 1] == cadena[i]:
+                return False
+            valor_par = valores[cadena[i+1]] - valores[cadena[i]]
+            if i + 2 < len(cadena) and valores[cadena[i+2]] > valor_par:
+                return False
+            i += 2
+        else:
+            if valores[cadena[i]] < valores[cadena[i+1]]:
+                return False
+            i += 1
+    return True
